85162ed478c10bf7987dbecece0d03a3b16607ca
xuekai-china <ee_shadow@163.com>
2021-04-12 04:52:48 +0800

    fix: not write LICENSE file

--- packages/create-app/index.js
+++ packages/create-app/index.js
@@ -100,6 +100,10 @@ function writeInfoFile(templatePackage) {
 .DS_Store
 node_modules
 dist
+build
+
+/.pnp
+.pnp.js
 
 # local env files
 .env.local
@@ -119,30 +123,6 @@ pnpm-debug.log*
 *.njsproj
 *.sln
 *.sw?`)
-
-  fs.writeFileSync(`${root}/LICENSE`, `
-MIT License
-
-Copyright (c) 2021, xuek <ee_shadow@163.com>.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.`)
-
 }
 
 function install(templatePackage) {
